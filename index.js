eaa7399defcb5f249cb200e7fbfc4cb317732141
mertushka <mertushkaonline@gmail.com>
2021-07-14 23:23:44 +0300

    Update

--- index.js
+++ index.js
@@ -12,10 +12,19 @@ room.on("message", (player, message) => {
 
 room.on("join", (player) => {
     console.log(`${player.name} joined the room!`)
+
+    //Example auth
+    let password = "1234" // ex. your value fetched from mongodb
+
+    room.send("login", player.name, password) // send the values to the client
 });
 
 room.on("error", (error) => {
     console.log(error)
+});
+
+room.on("console", (log) => {
+    console.log(log)
 });
 
 room.create("TOKEN")
--- room.js
+++ room.js
@@ -12,6 +12,21 @@ room.setScoreLimit(3);
 room.setTimeLimit(3);
 room.setTeamsLock(true);
 
+// Custom function that will be triggered when .send() function called
+onHaxballJSMessage = function (message) {
+    switch (message[0]) {
+        case 'login':
+            let playerName = message[1];
+            let password = message[2]
+            console.log(`Player: ${playerName} Password: ${password}`)
+            break;
+    
+        default:
+            console.log("Invalid message!")
+            break;
+    }
+}
+
 room.onPlayerChat = function (player, message) {
     emit("message", player, message) //Emitting custom events with haxball.js
 }
